7cbfd78d102ede834b5c084ee63fd9cdb331689e
Sauli Anto <sauli.anto@gmail.com>
2019-10-02 10:35:47 +0300

    Lisätty puuttuvat kentät

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/dto/peruste/lops2019/oppiaineet/Lops2019OppiaineKaikkiDto.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/dto/peruste/lops2019/oppiaineet/Lops2019OppiaineKaikkiDto.java
@@ -1,6 +1,7 @@
 package fi.vm.sade.eperusteet.ylops.dto.peruste.lops2019.oppiaineet;
 
 import fi.vm.sade.eperusteet.ylops.dto.peruste.lops2019.oppiaineet.moduuli.Lops2019ModuuliDto;
+import fi.vm.sade.eperusteet.ylops.dto.teksti.LokalisoituTekstiDto;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,8 @@ import java.util.List;
 @Getter
 @Setter
 public class Lops2019OppiaineKaikkiDto extends Lops2019OppiaineBaseDto {
+    private LokalisoituTekstiDto pakollisetModuulitKuvaus;
+    private LokalisoituTekstiDto valinnaisetModuulitKuvaus;
     private List<Lops2019ModuuliDto> moduulit = new ArrayList<>();
     private List<Lops2019OppiaineKaikkiDto> oppimaarat = new ArrayList<>();
 }
