2e39b65bdb8b2923a8aacd1d837a824bd8485d32
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-05-25 09:48:30 +0300

    EP-2813

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/domain/validation/ValidHtmlValidatorBase.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/domain/validation/ValidHtmlValidatorBase.java
@@ -17,10 +17,13 @@ package fi.vm.sade.eperusteet.ylops.domain.validation;
 
 import fi.vm.sade.eperusteet.ylops.domain.teksti.Kieli;
 import fi.vm.sade.eperusteet.ylops.domain.teksti.LokalisoituTeksti;
+import org.apache.commons.validator.routines.UrlValidator;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.safety.Whitelist;
 
 import java.util.Map;
+import org.jsoup.select.Elements;
 
 /**
  * @author mikkom
@@ -28,6 +31,7 @@ import java.util.Map;
 public abstract class ValidHtmlValidatorBase {
 
     private Whitelist whitelist;
+    private UrlValidator validator = new UrlValidator(UrlValidator.ALLOW_LOCAL_URLS);
 
     protected void setupValidator(ValidHtml constraintAnnotation) {
         whitelist = constraintAnnotation.whitelist().getWhitelist();
@@ -38,9 +42,15 @@ public abstract class ValidHtmlValidatorBase {
             Map<Kieli, String> tekstit = lokalisoituTeksti.getTeksti();
             if (tekstit != null) {
                 return tekstit.values().stream()
-                        .allMatch(teksti -> Jsoup.isValid(teksti, whitelist));
+                        .allMatch(teksti -> Jsoup.isValid(teksti, whitelist) && isValidUrls(teksti));
             }
         }
         return true;
+    }
+
+    private boolean isValidUrls(String teksti) {
+        Document doc = Jsoup.parse(teksti);
+        Elements links = doc.select("a[href]");
+        return links.stream().allMatch(link -> validator.isValid(link.attr("abs:href")));
     }
 }
