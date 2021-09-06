a0186448f2ae4e045b0280109101d5c592964d69
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-09-06 16:20:43 +0300

    EP-2692

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/domain/validation/ValidHtml.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/domain/validation/ValidHtml.java
@@ -53,7 +53,7 @@ public @interface ValidHtml {
                 .addAttributes("table", "align", "border", "cellpadding", "cellspacing", "style", "summary")
                 .addAttributes("th", "scope", "colspan", "rowspan", "style")
                 .addAttributes("td", "colspan", "rowspan", "style", "style", "data-colwidth")
-                .addAttributes("a", "href", "target", "rel")
+                .addAttributes("a", "href", "target", "rel", "routenode")
                 .addAttributes("img", "data-uid", "alt", "style", "src", "figcaption")
                 .addAttributes("figure", "class")
                 .addAttributes("abbr", "data-viite")
