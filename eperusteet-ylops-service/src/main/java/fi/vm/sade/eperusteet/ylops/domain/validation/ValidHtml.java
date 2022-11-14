67e1526c714b9009025af13d45352cdad4ee7cab
Timo Paasila <timo.paasila@gofore.com>
2022-11-14 10:37:23 +0200

    EP-3422

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/domain/validation/ValidHtml.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/domain/validation/ValidHtml.java
@@ -53,7 +53,7 @@ public @interface ValidHtml {
                 .addAttributes("table", "align", "border", "cellpadding", "cellspacing", "style", "summary")
                 .addAttributes("th", "scope", "colspan", "rowspan", "style")
                 .addAttributes("td", "colspan", "rowspan", "style", "data-colwidth")
-                .addAttributes("a", "href", "target", "rel", "routenode")
+                .addAttributes("a", "href", "target", "rel")
                 .addAttributes("img", "data-uid", "src", "alt", "height", "width", "style", "figcaption")
                 .addAttributes("abbr", "data-viite")
                 .addAttributes("figure", "class")
