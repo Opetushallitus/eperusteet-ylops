7e7a7d4be5b6eaa0f26f13f109c31510b6860bd1
Timo Paasila <timo.paasila@gofore.com>
2023-04-28 13:04:41 +0300

    EP-3680

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/service/ops/impl/TekstiKappaleViiteServiceImpl.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/service/ops/impl/TekstiKappaleViiteServiceImpl.java
@@ -134,15 +134,18 @@ public class TekstiKappaleViiteServiceImpl implements TekstiKappaleViiteService 
         TekstiKappaleViiteDto.Matala tekstiKappaleViite = getTekstiKappaleViite(opsId, viiteId);
 
         if (perusteDto.getTekstiKappaleViiteSisalto() != null) {
-            return CollectionUtil.treeToStream(
-                    perusteDto.getTekstiKappaleViiteSisalto(),
-                    fi.vm.sade.eperusteet.ylops.service.external.impl.perustedto.TekstiKappaleViiteDto::getLapset)
-                    .filter(viiteDto -> viiteDto.getTekstiKappale() != null && tekstiKappaleViite != null
-                            && Objects.equals(tekstiKappaleViite.getPerusteTekstikappaleId(), viiteDto.getTekstiKappale().getId()))
-                    .findFirst()
-                    .orElse(null);
+            try {
+                return CollectionUtil.treeToStream(
+                                perusteDto.getTekstiKappaleViiteSisalto(),
+                                fi.vm.sade.eperusteet.ylops.service.external.impl.perustedto.TekstiKappaleViiteDto::getLapset)
+                        .filter(viiteDto -> viiteDto.getTekstiKappale() != null && tekstiKappaleViite != null
+                                && Objects.equals(tekstiKappaleViite.getPerusteTekstikappaleId(), viiteDto.getTekstiKappale().getId()))
+                        .findFirst()
+                        .orElse(null);
+            } catch (Exception e) {
+                // filtteröinti voi palauttaa null ja findFirst heittää poikkeuksen
+            }
         }
-
         return null;
     }
 
