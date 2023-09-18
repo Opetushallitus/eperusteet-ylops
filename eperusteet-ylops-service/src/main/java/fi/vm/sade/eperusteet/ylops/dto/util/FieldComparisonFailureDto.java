a6467599112e3ba998a1fcd70f6e528441fc238d
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2023-09-18 10:41:42 +0300

    bugfix/EP-3808

---
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/dto/util/FieldComparisonFailureDto.java
@@ -0,0 +1,12 @@
+package fi.vm.sade.eperusteet.ylops.dto.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FieldComparisonFailureDto {
+    private String field;
+}
