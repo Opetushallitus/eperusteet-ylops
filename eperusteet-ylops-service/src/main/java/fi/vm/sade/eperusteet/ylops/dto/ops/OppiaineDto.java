d6870bf4ed5123204cb94bce35ddf9bcdd0b81ab
Timo Paasila <113356513+timopaasila@users.noreply.github.com>
2024-01-02 15:39:34 +0200

    EP-4034 (#731)
    EP-4034

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/dto/ops/OppiaineDto.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/dto/ops/OppiaineDto.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2013 The Finnish Board of Education - Opetushallitus
- *
- * This program is free software: Licensed under the EUPL, Version 1.1 or - as
- * soon as they will be approved by the European Commission - subsequent versions
- * of the EUPL (the "Licence");
- *
- * You may not use this work except in compliance with the Licence.
- * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * European Union Public Licence for more details.
- */
 package fi.vm.sade.eperusteet.ylops.dto.ops;
 
 import fi.vm.sade.eperusteet.ylops.dto.ReferenceableDto;
@@ -22,12 +7,11 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 
-/**
- * @author mikkom
- */
 @Data
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
@@ -42,4 +26,5 @@ public class OppiaineDto extends OppiaineBaseDto implements ReferenceableDto {
     private String muokkaaja;
     private Date muokattu;
     private OppiaineDto pohjanOppiaine;
+    private List<VapaatekstiPaikallinentarkennusDto> vapaatTekstit = new ArrayList<>();
 }
