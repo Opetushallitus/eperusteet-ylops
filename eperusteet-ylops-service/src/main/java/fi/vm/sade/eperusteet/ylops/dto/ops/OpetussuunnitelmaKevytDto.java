f15480ed811c5d5bfe36cd4d1555fe00d2298b93
Timo Paasila <timo.paasila@gofore.com>
2024-02-02 12:54:29 +0200

    EP-4099

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/dto/ops/OpetussuunnitelmaKevytDto.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/dto/ops/OpetussuunnitelmaKevytDto.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2013 The Finnish Board of Education - Opetushallitus
- *
- * This program is free software: Licensed under the EUPL, Version 1.1 or - as
- * soon as they will be approved by the European Commission - subsequent versions
- * of the EUPL (the "Licence");
- *
- * You may not use this work except in compliance with the Licence.
- * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * European Union Public Licence for more details.
- */
 package fi.vm.sade.eperusteet.ylops.dto.ops;
 
 import lombok.AllArgsConstructor;
@@ -20,11 +5,10 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
-/**
- * @author nkala
- */
 @Data
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
@@ -33,4 +17,5 @@ public class OpetussuunnitelmaKevytDto extends OpetussuunnitelmaBaseDto {
     private OpetussuunnitelmaBaseDto pohja;
     private Set<OpsVuosiluokkakokonaisuusKevytDto> vuosiluokkakokonaisuudet;
     private Set<OpsOppiaineKevytDto> oppiaineet;
+    private List<OpetussuunnitelmaNimiDto> periytyvatPohjat = new ArrayList<>();
 }
