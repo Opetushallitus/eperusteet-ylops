e90402b615c28c249b537c185ec00f7fda6168c9
Timo Paasila <timo.paasila@gofore.com>
2024-02-16 09:27:57 +0200

    EP-4102

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/service/mapping/DtoMapperConfig.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/service/mapping/DtoMapperConfig.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2013 The Finnish Board of Education - Opetushallitus
- *
- * This program is free software: Licensed under the EUPL, Version 1.1 or - as
- * soon as they will be approved by the European Commission - subsequent versions
- * of the EUPL (the "Licence");
- *
- * You may not use this work except in compliance with the Licence.
- * You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * European Union Public Licence for more details.
- */
 package fi.vm.sade.eperusteet.ylops.service.mapping;
 
 import fi.vm.sade.eperusteet.ylops.domain.Tila;
@@ -43,7 +28,6 @@ import fi.vm.sade.eperusteet.ylops.dto.ops.OppiaineLaajaDto;
 import fi.vm.sade.eperusteet.ylops.dto.ops.PoistettuOppiaineDto;
 import fi.vm.sade.eperusteet.ylops.dto.peruste.lukio.LukioPerusteOppiaineDto;
 import fi.vm.sade.eperusteet.ylops.dto.teksti.LokalisoituTekstiDto;
-import fi.vm.sade.eperusteet.ylops.repository.teksti.LokalisoituTekstiRepository;
 import fi.vm.sade.eperusteet.ylops.service.external.impl.perustedto.PerusteenLokalisoituTekstiDto;
 import fi.vm.sade.eperusteet.ylops.service.external.impl.perustedto.TekstiKappaleDto;
 import ma.glasnost.orika.CustomMapper;
@@ -51,21 +35,14 @@ import ma.glasnost.orika.MappingContext;
 import ma.glasnost.orika.converter.builtin.PassThroughConverter;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 import org.apache.commons.collections.CollectionUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Instant;
 import java.util.Comparator;
 
-/**
- * @author jhyoty
- */
 @Configuration
 public class DtoMapperConfig {
-    @Autowired
-    private LokalisoituTekstiRepository lokalisoituTekstiRepository;
-
     @Bean
     public DtoMapper dtoMapper(
             ReferenceableEntityConverter referenceableEntityConverter,
@@ -73,6 +50,7 @@ public class DtoMapperConfig {
             PerusteenLokalisoituTekstiConverter perusteenLokalisoituTekstiConverter,
             PerusteenLokalisoituTekstiToLokalisoituTekstiConverter perusteenLokalisoituTekstiToLokalisoituTekstiConverter,
             KoodistoKoodiConverter koodistoKoodiConverter) {
+
         DefaultMapperFactory factory = new DefaultMapperFactory.Builder()
                 .build();
 
@@ -149,10 +127,6 @@ public class DtoMapperConfig {
                 .exclude("tyyppi")
                 .byDefault()
                 .register();
-
-//        factory.classMap(KommenttiKahvaDto.class, KommenttiKahva.class)
-//                .byDefault()
-//                .register();
 
         factory.classMap(PoistettuOppiaineDto.class, Lops2019PoistettuDto.class)
                 .fieldAToB("oppiaine", "poistettuId")
