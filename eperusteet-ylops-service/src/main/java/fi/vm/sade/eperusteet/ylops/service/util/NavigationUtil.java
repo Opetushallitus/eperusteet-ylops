eaa0395b8868de678607ca55fa76be799643c921
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2024-08-21 10:26:04 +0300

    EP-4321-2

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/service/util/NavigationUtil.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/service/util/NavigationUtil.java
@@ -2,7 +2,6 @@ package fi.vm.sade.eperusteet.ylops.service.util;
 
 import fi.vm.sade.eperusteet.ylops.dto.navigation.NavigationNodeDto;
 import fi.vm.sade.eperusteet.ylops.dto.navigation.NavigationType;
-import fi.vm.sade.eperusteet.ylops.dto.ops.OpetussuunnitelmaKevytDto;
 import lombok.experimental.UtilityClass;
 
 import java.util.List;
@@ -14,7 +13,7 @@ public class NavigationUtil {
 
     private static final Set<NavigationType> NUMEROITAVAT_TYYPIT = Set.of(NavigationType.viite);
 
-    public static NavigationNodeDto asetaNumerointi(OpetussuunnitelmaKevytDto opetussuunnitelma, NavigationNodeDto node) {
+    public static NavigationNodeDto asetaNumerointi(NavigationNodeDto node) {
         asetaNumerointi(node.getChildren(), "");
         return node;
     }
