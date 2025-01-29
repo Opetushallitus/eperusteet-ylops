892c11ae5d4cd694979865020c8ee873e3c41fad
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2025-01-29 20:34:47 +0200

    fix/EP-4379-5

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/dto/peruste/PerusteOpetuksentavoiteDto.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/dto/peruste/PerusteOpetuksentavoiteDto.java
@@ -1,5 +1,6 @@
 package fi.vm.sade.eperusteet.ylops.dto.peruste;
 
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import fi.vm.sade.eperusteet.ylops.dto.ReferenceableDto;
 import fi.vm.sade.eperusteet.ylops.dto.teksti.LokalisoituTekstiDto;
@@ -18,7 +19,9 @@ public class PerusteOpetuksentavoiteDto implements ReferenceableDto {
     private Long id;
     private UUID tunniste;
     private LokalisoituTekstiDto tavoite;
+    @JsonIdentityReference(alwaysAsId = true)
     private Set<PerusteKeskeinensisaltoalueDto> sisaltoalueet;
+    @JsonIdentityReference(alwaysAsId = true)
     @JsonProperty("laajaalaisetosaamiset")
     private Set<PerusteLaajaalainenosaaminenDto> laajattavoitteet;
     private Set<PerusteOpetuksenkohdealueDto> kohdealueet;
