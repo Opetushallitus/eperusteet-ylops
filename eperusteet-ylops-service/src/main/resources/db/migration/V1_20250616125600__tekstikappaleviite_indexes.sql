675960860bfcfb9e7d43a517f33b4903bfd200a1
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2025-06-16 13:01:45 +0300

    EP-4759

--- eperusteet-ylops-service/src/main/resources/db/migration/V1_20250616125600__tekstikappaleviite_indexes.sql
+++ eperusteet-ylops-service/src/main/resources/db/migration/V1_20250616125600__tekstikappaleviite_indexes.sql
@@ -1,4 +1,4 @@
-CREATE INDEX IF NOT EXISTS tekstikappaleviite_vanhempi_id_indx on tekstikappaleviite (vanhempi_id);
-CREATE INDEX IF NOT EXISTS tekstikappaleviite_tekstikappale_id_indx on tekstikappaleviite (tekstikappale_id);
-CREATE INDEX IF NOT EXISTS tekstikappale_tunniste_indx on tekstikappale (tunniste);
+CREATE INDEX IF NOT EXISTS tekstikappaleviite_vanhempi_id_index on tekstikappaleviite (vanhempi_id);
+CREATE INDEX IF NOT EXISTS tekstikappaleviite_tekstikappale_id_index on tekstikappaleviite (tekstikappale_id);
+CREATE INDEX IF NOT EXISTS tekstikappale_tunniste_index on tekstikappale (tunniste);
 CREATE INDEX IF NOT EXISTS opetussuunnitelma_tekstit_id_index on opetussuunnitelma (tekstit_id);
