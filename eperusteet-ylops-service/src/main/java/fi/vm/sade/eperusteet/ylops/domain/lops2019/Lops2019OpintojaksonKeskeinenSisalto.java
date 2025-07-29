b19e7f818b76f739743700cb9735ade16c63f223
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2025-07-29 10:44:50 +0300

    bugfix/EP-4761

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/domain/lops2019/Lops2019OpintojaksonKeskeinenSisalto.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/domain/lops2019/Lops2019OpintojaksonKeskeinenSisalto.java
@@ -3,15 +3,14 @@ package fi.vm.sade.eperusteet.ylops.domain.lops2019;
 import fi.vm.sade.eperusteet.ylops.domain.AbstractAuditedReferenceableEntity;
 import fi.vm.sade.eperusteet.ylops.domain.teksti.LokalisoituTeksti;
 import fi.vm.sade.eperusteet.ylops.domain.validation.ValidHtml;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.envers.Audited;
-import org.hibernate.envers.RelationTargetAuditMode;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.RelationTargetAuditMode;
 
 @Entity
 @Audited
@@ -23,4 +22,13 @@ public class Lops2019OpintojaksonKeskeinenSisalto extends AbstractAuditedReferen
     @Setter
     @ValidHtml(whitelist = ValidHtml.WhitelistType.NORMAL)
     private LokalisoituTeksti kuvaus;
+
+    public static Lops2019OpintojaksonKeskeinenSisalto copy(Lops2019OpintojaksonKeskeinenSisalto original) {
+        if (original == null) {
+            return null;
+        }
+        Lops2019OpintojaksonKeskeinenSisalto result = new Lops2019OpintojaksonKeskeinenSisalto();
+        result.setKuvaus(original.getKuvaus());
+        return result;
+    }
 }
--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/domain/lops2019/Lops2019OpintojaksonTavoite.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/domain/lops2019/Lops2019OpintojaksonTavoite.java
@@ -3,15 +3,14 @@ package fi.vm.sade.eperusteet.ylops.domain.lops2019;
 import fi.vm.sade.eperusteet.ylops.domain.AbstractAuditedReferenceableEntity;
 import fi.vm.sade.eperusteet.ylops.domain.teksti.LokalisoituTeksti;
 import fi.vm.sade.eperusteet.ylops.domain.validation.ValidHtml;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.envers.Audited;
-import org.hibernate.envers.RelationTargetAuditMode;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.envers.Audited;
+import org.hibernate.envers.RelationTargetAuditMode;
 
 @Entity
 @Audited
@@ -24,4 +23,13 @@ public class Lops2019OpintojaksonTavoite extends AbstractAuditedReferenceableEnt
     @Setter
     @ValidHtml(whitelist = ValidHtml.WhitelistType.NORMAL)
     private LokalisoituTeksti kuvaus;
+
+    public static Lops2019OpintojaksonTavoite copy(Lops2019OpintojaksonTavoite original) {
+        if (original == null) {
+            return null;
+        }
+        Lops2019OpintojaksonTavoite result = new Lops2019OpintojaksonTavoite();
+        result.setKuvaus(original.getKuvaus());
+        return result;
+    }
 }
