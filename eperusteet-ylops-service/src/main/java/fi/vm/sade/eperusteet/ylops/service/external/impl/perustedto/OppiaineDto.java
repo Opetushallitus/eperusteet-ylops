4d999db8b38247b96f03fc11a2de30a566eef46c
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2025-11-28 10:10:36 +0200

    EP-4865

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/service/external/impl/perustedto/OppiaineDto.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/service/external/impl/perustedto/OppiaineDto.java
@@ -1,10 +1,13 @@
 package fi.vm.sade.eperusteet.ylops.service.external.impl.perustedto;
 
+import fi.vm.sade.eperusteet.ylops.dto.Reference;
+import fi.vm.sade.eperusteet.ylops.dto.peruste.PerusteOppiaineenVuosiluokkakokonaisuusDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
@@ -25,4 +28,14 @@ public class OppiaineDto {
     private Set<OppiaineenVuosiluokkaKokonaisuusDto> vuosiluokkakokonaisuudet;
     private String koodiUri;
     private String koodiArvo;
+
+    public Optional<OppiaineenVuosiluokkaKokonaisuusDto> getVuosiluokkakokonaisuus(Reference tunniste) {
+        return getVuosiluokkakokonaisuus(UUID.fromString(tunniste.toString()));
+    }
+
+    public Optional<OppiaineenVuosiluokkaKokonaisuusDto> getVuosiluokkakokonaisuus(UUID tunniste) {
+        return vuosiluokkakokonaisuudet.stream()
+                .filter(v -> v.getVuosiluokkaKokonaisuus().getTunniste().equals(tunniste))
+                .findAny();
+    }
 }
