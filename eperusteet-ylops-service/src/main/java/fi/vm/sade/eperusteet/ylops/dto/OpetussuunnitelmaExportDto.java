48fbee0987fc373b80501169f992a0493c00dbb8
Olli-Pekka Lehtokallio <53811784+oplekal@users.noreply.github.com>
2026-06-26 15:41:26 +0300

    EP-5051 (#948)

--- eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/dto/OpetussuunnitelmaExportDto.java
+++ eperusteet-ylops-service/src/main/java/fi/vm/sade/eperusteet/ylops/dto/OpetussuunnitelmaExportDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import fi.vm.sade.eperusteet.ylops.dto.ops.OpetussuunnitelmaBaseDto;
 import fi.vm.sade.eperusteet.ylops.dto.ops.OpetussuunnitelmaNimiDto;
 import fi.vm.sade.eperusteet.ylops.dto.peruste.PerusteInfoDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,8 +15,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Schema(description = "Julkaistun opetussuunnitelman sisältö julkiseen rajapintaan.")
 public class OpetussuunnitelmaExportDto extends OpetussuunnitelmaBaseDto {
+
+    @Schema(description = "Opetussuunnitelman perusteen tiedot julkaisuhetken version mukaan.")
     private PerusteInfoDto peruste;
+
+    @Schema(description = "Pohjaopetussuunnitelman nimitiedot, jos opetussuunnitelma on luotu pohjalle.")
     private OpetussuunnitelmaNimiDto pohja;
+
+    @Schema(description = "Opetussuunnitelman tekstirakenne puumuotona.")
     private TekstiKappaleViiteExportDto.Puu tekstit;
 }
